524049b63e5a6ab16b0f195f59e01c71a46fab91
leticiacisz <161315361+leticiacisz@users.noreply.github.com>
2024-05-15 07:37:55 -0300

    Add files via upload
    lista de exercicios.Atividade 13

---
+++ java.js
@@ -0,0 +1,19 @@
+function diferenca () {
+   var PrimeiraVariavel = parseInt (document.getElementById) ('var1').value;
+   var SegundaVariavel = parseInt (document.getElementById) ('var2').value;
+   var PrimeiroResultado = document.querySelector ('span');
+
+if (Primeiravariavel > segundavariavel) {
+    resultadoUm = PrimeiraVariavel - SegundaVariavel 
+
+} else {
+    resultadoDois = SegundaVariavel - PrimeiraVariavel 
+}
+
+    console.log (resultado)
+
+}
+
+function dobro () {
+
+}
